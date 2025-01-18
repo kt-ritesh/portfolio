f00423b2b2a19b8155ce548c84423103a55d46cd
Ritesh Kr <ritesh123kumar081@gmail.com>
2025-01-19 00:00:12 +0530

    hamburger
    Signed-off-by: Ritesh Kr <ritesh123kumar081@gmail.com>

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ window.addEventListener("scroll", function(){
     var navbar = document.querySelector("nav");
     navbar.classList.toggle("sticky", window.scrollY > 0);
 })
+
+const hamburger = document.querySelector("#hamburger");
+const navLink = document.querySelector(".nav-links");
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active");
+    navLink.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+    hamburger.classList.remove("active");
+    navLink.classList.remove("active");
+}))
+
